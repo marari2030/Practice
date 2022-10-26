a16977cf3e5c4124846f7fa1184d53036621a29d
LeeChinese <115471662+LeeChinese@users.noreply.github.com>
2022-10-26 22:51:39 +0800

    分支语句if-else

--- test11.c
+++ test11.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 
-int main()
-{
-	int a = 1;
-	int i = 1;
-	while (i <= 100)
-	{
-		if (a % 2 == 1)
-		{
-			printf("%d\n", a);
-		}
-		i++;
-		a++;
-
-	}
-
-	
-
-	return 0;
-}
+//int main()
+//{
+//	int a = 1;
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			printf("%d\n", a);
+//		}
+//		i++;
+//		a++;
+//
+//	}
+//
+//	
+//
+//	return 0;
+//}
